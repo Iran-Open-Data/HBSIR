1e6ba1e114125919c9a0207400d6fe909f1e3487
mohammad amin alavian <m.amin.alavian@gmail.com>
2023-12-16 14:42:16 +0330

    Update docs

--- hbsir/calculate.py
+++ hbsir/calculate.py
@@ -1,10 +1,21 @@
 """
 This module contains functions for calculating various metrics.
 """
+# pylint: disable=too-many-arguments
+# pylint: disable=unused-argument
+# pylint: disable=too-many-locals
+
+from typing import Iterable
 
 import pandas as pd
 
+from bssir.calculator.quantile import _EquivalenceScale, _QuantileBase
+
 from .api import api
+
+
+def __get_optional_params(local_variables: dict) -> dict:
+    return {key: value for key, value in local_variables.items() if value is not None}
 
 
 def weighted_average(
@@ -28,11 +39,8 @@ def weighted_average(
     pd.Series
         The weighted average of the given columns.
     """
-    api.calculate.weighted_average(
-        table=table,
-        weight_col=weight_col,
-        columns=columns,
-    )
+    parameters = __get_optional_params(locals())
+    return api.calculate.weighted_average(**parameters)
 
 
 def average_table(
@@ -59,9 +67,42 @@ def average_table(
     pd.DataFrame
         The weighted average of the given columns for each group.
     """
-    api.calculate.average_table(
-        table=table,
-        weight_col=weight_col,
-        groupby=groupby,
-        columns=columns,
-    )
+    parameters = __get_optional_params(locals())
+    return api.calculate.average_table(**parameters)
+
+
+def quantile(
+    table: pd.DataFrame | pd.Series | None = None,
+    quantile_column_name: str = "Quantile",
+    bins: int = -1,
+    weight_column: str | None = None,
+    on_variable: _QuantileBase | None = None,
+    on_column: str | None = None,
+    weighted: bool = True,
+    adjust_weight_for_household_size: bool = False,
+    equivalence_scale: _EquivalenceScale = "Household",
+    for_all: bool = True,
+    annual: bool = True,
+    groupby: str | Iterable[str] | None = None,
+    years: list[int] | None = None,
+) -> pd.Series:
+    parameters = __get_optional_params(locals())
+    return api.calculate.quantile(**parameters)
+
+
+def add_decile(
+    table: pd.DataFrame | pd.Series | None = None,
+    quantile_column_name: str = "Quantile",
+    weight_column: str | None = None,
+    on_variable: _QuantileBase | None = None,
+    on_column: str | None = None,
+    weighted: bool = True,
+    adjust_weight_for_household_size: bool = False,
+    equivalence_scale: _EquivalenceScale = "Household",
+    for_all: bool = True,
+    annual: bool = True,
+    groupby: str | Iterable[str] | None = None,
+    years: list[int] | None = None,
+) -> pd.Series:
+    parameters = __get_optional_params(locals())
+    return api.calculate.add_decile(**parameters)
