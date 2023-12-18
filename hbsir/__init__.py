87108d4b6bcab713f9c661f24173828f2bca3757
mohammad amin alavian <m.amin.alavian@gmail.com>
2023-12-19 01:09:37 +0330

    Add package docstring

--- hbsir/__init__.py
+++ hbsir/__init__.py
@@ -1,3 +1,14 @@
+"""HBSIR - Tools for working with Household Budget Survey of Iran
+
+This package provides a set of tools to facilitate working with the 
+Household Budget Survey of Iran (HBSIR) data.
+  
+It allows easy access to standardized survey tables across multiple 
+years. It also enables data enrichment, aggregation, analysis and 
+visualization.
+
+"""
+
 from .api import (
     load_table,
     load_knowledge,
