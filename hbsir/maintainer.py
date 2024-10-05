b03562512794ccadc64d58f065d63f5d908bde87
mohammad amin alavian <m.amin.alavian@gmail.com>
2024-10-06 00:59:10 +0330

    Add maintainer

---
+++ hbsir/maintainer.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+from bssir.metadata_reader import config, _Years
+from bssir.api import API
+from bssir.maintainer import Maintainer
+
+
+defaults, metadata = config.set_package_config(Path(__file__).parent)
+api = API(defaults=defaults, metadata=metadata)
+maintainer = Maintainer(lib_defaults=defaults, lib_metadata=metadata)
+
+def create_and_upload(years: _Years) -> None:
+    api.setup(
+        years,
+        method="create_from_raw",
+        download_source="original",
+    )
+    maintainer.upload_raw_files()
+    maintainer.upload_cleaned_files()
